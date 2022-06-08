e30d81f9491e37f439cf123f20be8695c769f11e
FrancisBega <francis.bega@ttcl.co.tz>
2022-06-08 10:31:54 -0700

    low level

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <main.h>
+
+/**
+ * main - ENtry Point
+ *
+ * Return: Alwasys Return 0
+ */
+
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_puchar('\n');
+return (0);
+}
