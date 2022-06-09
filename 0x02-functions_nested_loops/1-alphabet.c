4cb96b81b53f6067e9977ad1d490209501c16496
FrancisBega <francis.bega@ttcl.co.tz>
2022-06-09 03:50:30 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+
+
+/**
+
+ * print_alphabet - Entry Point
+
+ */
+
+
+
+void print_alphabet(void)
+
+{
+
+	char c = 'a';
+
+
+
+	while (c <= 'z')
+
+	{
+
+		_putchar(c);
+
+		c++;
+
+	}
+
+	_putchar('\n');
+
+
+
+}
